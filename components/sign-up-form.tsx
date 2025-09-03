fdd61f1d905f57f747ee4b9c012638450df77894
Richard H. <richard@haddads.net.au>
2025-09-03 20:24:00 +1000

    fix redirect

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -120,7 +120,7 @@ export function SignUpForm({
         email,
         password,
         options: {
-          emailRedirectTo: `${window.location.origin}/protected`,
+          emailRedirectTo: `${window.location.origin}/dashboard`,
           data: {
             username: username, // Pass the input username
           },
